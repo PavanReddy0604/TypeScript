5d834d0cc0cb445e1f52b1222b4cc53fb5e15b8b
Pavan <pavankumarreddy977@gmail.com>
2024-08-15 11:17:44 +0530

    started with TS

---
+++ basics.ts
@@ -0,0 +1,23 @@
+
+console.log("Hello World.!")
+
+//Explicit type 
+let firstName:string="pavan"
+console.log(typeof firstName)
+
+//Implicit type 
+let lastName="Reddy"
+console.log(typeof lastName)
+console.log(lastName.toUpperCase())
+
+let empId:number=123
+console.log(typeof empId)
+
+let empSalary:number=123.34
+console.log(typeof empSalary)
+
+let isActive:boolean=true
+console.log(typeof isActive)
+
+
+
